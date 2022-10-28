496d6c1bef3c0f3253d6dbab59bddbff41880515
Artyom Abramyan <artemabramyan7@icloud.com>
2022-10-28 20:40:19 +0300

    [Common/feat] Добавил класс Randomness

---
+++ Common/Random.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common;
+
+public static class Randomness
+{
+	private static readonly Random Random = new();
+
+	public static int GetInt() => Random.Next();
+	public static int GetInt(int max) => Random.Next(max);
+	public static int GetInt(int min, int max) => Random.Next(min, max);
+	public static double GetDouble() => Random.NextDouble();
+}
