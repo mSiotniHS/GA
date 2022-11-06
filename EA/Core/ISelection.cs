aa29d866300fe237abd5d98ab2b0c49f5a12f14b
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:35:44 +0300

    [EA/feat] Определил интерфейс селекции ISelection.cs

---
+++ EA/Core/ISelection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EA.Core;
+
+public interface ISelection
+{
+	public IEnumerable<Genotype> Perform(List<Genotype> fund, Func<Genotype, int> phenotype,
+		uint count);
+}
