9facb0daef905e9e6de9f0505382dda312725585
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 18:08:37 +0300

    [EA/feat] Создал класс, завершающий работу ЭГА при достижении определённого поколения

---
+++ EA/CommonModules/GenerationCountEvaluator.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.CommonModules;
+
+public sealed class GenerationCountEvaluator<TBase> : IEvaluationStrategy<TBase>
+{
+	private readonly int _maxGenerations;
+
+	public GenerationCountEvaluator(int maxGenerations)
+	{
+		_maxGenerations = maxGenerations;
+	}
+
+	public bool ShouldWork(GaManager<TBase> state)
+	{
+		return _maxGenerations != state.Statistics.TotalGenerationCount;
+	}
+}
