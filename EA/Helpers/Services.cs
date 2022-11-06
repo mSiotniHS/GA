168efe7e807b88abb099ace82d3adacfd04a2967
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:36:54 +0300

    [EA/feat] Определил вспомогательный класс Services.cs

---
+++ EA/Helpers/Services.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using EA.Core;
+
+namespace EA.Helpers;
+
+internal static class Services
+{
+	public static Genotype FindBest(IEnumerable<Genotype> group, Func<Genotype, int> fitness)
+	{
+		Genotype best = null;
+		foreach (var genotype in group)
+		{
+			if (best is null || fitness(genotype) > fitness(best))
+			{
+				best = genotype;
+			}
+		}
+
+		return best ?? throw new Exception("can't be right");
+	}
+}
