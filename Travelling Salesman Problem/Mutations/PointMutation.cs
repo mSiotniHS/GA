ce148e99cf75ed46f4b5b6c06187c52a87bcf79e
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 18:07:35 +0300

    [TSP/feat] Реализовал точечную мутацию

---
+++ Travelling Salesman Problem/Mutations/PointMutation.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem.Mutations;
+
+public sealed class PointMutation : IMutation
+{
+	public Genotype Perform(Genotype genotype)
+	{
+		var startIdx = Randomness.GetInt(genotype.Length - 1);
+
+		var newGenotype = new Genotype(genotype);
+		(newGenotype[startIdx], newGenotype[startIdx + 1]) = (newGenotype[startIdx + 1], newGenotype[startIdx]);
+
+		return newGenotype;
+	}
+
+	public bool GuaranteesValidGenotype => true;
+}
