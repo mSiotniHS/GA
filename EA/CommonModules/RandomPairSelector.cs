e2508ce31dc28b2eca55ccabc1f80eab29e4454d
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:45:41 +0300

    [EA/feat] Определил класс, произвольно выбирающий родителей

---
+++ EA/CommonModules/RandomPairSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Common;
+using EA.Core;
+
+namespace EA.CommonModules;
+
+public sealed class RandomPairSelector : IPairSelector
+{
+	public IEnumerable<(Genotype, Genotype)> Select(IEnumerable<Genotype> population)
+	{
+		var populationList = population.ToList();
+		var pairs = new List<(Genotype, Genotype)>();
+
+		while (populationList.Count != 0)
+		{
+			var firstIdx = Randomness.GetInt(populationList.Count);
+			populationList.RemoveAt(firstIdx);
+			var secondIdx = Randomness.GetInt(populationList.Count);
+			populationList.RemoveAt(secondIdx);
+
+			pairs.Add((populationList[firstIdx], populationList[secondIdx]));
+		}
+
+		return pairs;
+	}
+}
