a50dcc57d5a7097b07efe3e180e1c40e4de9bf46
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:58:23 +0300

    [TSP/feat] Определил "буквальный" кодировщик городов

---
+++ Travelling Salesman Problem/BasicRouteCoder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EA;
+using EA.BaseProblem;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem;
+
+public sealed class BasicRouteCoder: ICoder<List<int>, Genotype>
+{
+	public Genotype Encode(List<int> from) => new (from.Cast<int?>());
+
+	public List<int> Decode(Genotype to)
+	{
+		var from = new List<int>();
+
+		for (var i = 0; i < to.Length; i++)
+		{
+			from.Add(to.GetNonNull(i));
+		}
+
+		return from;
+	}
+}
