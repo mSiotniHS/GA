fe153155b9a516e5fd3333bb767baffee2fab4f4
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:38:45 +0300

    [EA/feat] Написал реализацию бета-турнира

---
+++ EA/CommonModules/BetaTournament.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using EA.Core;
+using EA.Helpers;
+
+namespace EA.CommonModules;
+
+public sealed class BetaTournament : ISelection
+{
+	private readonly uint _beta;
+
+	public BetaTournament(uint beta)
+	{
+		_beta = beta;
+	}
+
+	public IEnumerable<Genotype> Perform(List<Genotype> fund, Func<Genotype, int> phenotype,
+		uint count)
+	{
+		var selected = new List<Genotype>();
+		for (var selectedCount = 0; selectedCount < count; selectedCount++)
+		{
+			selected.Add(Round(fund, phenotype));
+		}
+
+		return selected;
+	}
+
+	private Genotype Round(IReadOnlyList<Genotype> fund, Func<Genotype, int> phenotype)
+	{
+		var fighters = new List<Genotype>();
+		for (var i = 0; i < _beta; i++)
+		{
+			fighters.Add(fund[Randomness.GetInt(fund.Count)]);
+		}
+
+		return Services.FindBest(fighters, phenotype);
+	}
+}
