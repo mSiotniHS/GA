05a08e05478c7083dd7f06fc0502738abfe1a561
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 18:03:43 +0300

    [refac] Почистил using-и

--- EA/Helpers/StatisticsCommittee.cs
+++ EA/Helpers/StatisticsCommittee.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using EA.Core;
 
 namespace EA.Helpers;
--- Travelling Salesman Problem/TSP.cs
+++ Travelling Salesman Problem/TSP.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using EA;
 using EA.BaseProblem;
 using EA.Core;
 
