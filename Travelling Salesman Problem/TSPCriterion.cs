ccd8588287c946020263b6a79a91b6f7023a2028
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:46:27 +0300

    [TSP/feat] Определил класс, вычисляющий длину маршрута

---
+++ Travelling Salesman Problem/TSPCriterion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using EA.BaseProblem;
+
+namespace Travelling_Salesman_Problem;
+
+public sealed class TspCriterion: ICriterion<List<int>>
+{
+	private readonly DistanceMatrix _distances;
+
+	public TspCriterion(DistanceMatrix distances)
+	{
+		_distances = distances;
+	}
+
+	public int Calculate(List<int> bas)
+	{
+		var sum = 0;
+		for (var i = 0; i < bas.Count - 1; i++)
+		{
+			sum += _distances[bas[i], bas[i + 1]];
+		}
+		sum += _distances[bas[^1], bas[0]];
+
+		return sum;
+	}
+}
