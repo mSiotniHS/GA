f0dcccb2d32583eee5bebae977f200ad35e8e049
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 17:01:22 +0300

    [TSP/feat] Реализовал порядковый кроссовер

---
+++ Travelling Salesman Problem/Crossovers/OxCrossover.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem.Crossovers;
+
+// works!
+public sealed class OxCrossover: ICrossover
+{
+	public List<Genotype> Perform(Genotype parent1, Genotype parent2)
+	{
+		// [point1, point2)
+		var point1 = Randomness.GetInt(parent1.Length); // in [0, parent1.Length - 1]
+		var point2 = Randomness.GetInt(point1 + 1, parent1.Length+1); // in [point1 + 1, parent1.Length]
+
+		Console.WriteLine($"[{point1}, {point2})");
+
+		/*point1 = 2;
+		point2 = 5;*/
+
+		var child1 = Base(parent1, parent2, point1, point2);
+		var child2 = Base(parent2, parent1, point1, point2);
+
+		return new List<Genotype> { child1, child2 };
+	}
+
+	// interval [point1, point2)
+	private static Genotype Base(Genotype parent1, Genotype parent2, int point1, int point2)
+	{
+		var child = parent1.Copy(point1, point2);
+
+		var childIdx = point2;
+		var parent2Idx = point2;
+
+		int Inc(int idx) => (idx + 1) % parent1.Length;
+
+		while (childIdx != point1)
+		{
+			var gene = parent2.GetNonNull(parent2Idx);
+
+			if (!child.ContainsIn(gene, point1, point2))
+			{
+				child[childIdx] = gene;
+				childIdx = Inc(childIdx);
+			}
+
+			parent2Idx = Inc(parent2Idx);
+		}
+
+		return child;
+	}
+
+	public bool GuaranteesValidGenotype => true;
+}
