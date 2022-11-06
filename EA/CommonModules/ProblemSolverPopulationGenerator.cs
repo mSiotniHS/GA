188c857049a663dd0aff71892635abf278f93b2f
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:45:06 +0300

    [EA/feat] Определил класс, формирующий начальную популяцию по приближённому решению задачи

---
+++ EA/CommonModules/ProblemSolverPopulationGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using EA.BaseProblem;
+using EA.Core;
+
+namespace EA.CommonModules;
+
+public sealed class ProblemSolverPopulationGenerator<TBase, TGaProblem> : IPopulationGenerator
+	where TGaProblem : IGaProblem<TBase>
+{
+	private TGaProblem BaseProblem { get; }
+	private IProblemSolver<TBase, TGaProblem> ProblemSolver { get; }
+
+	public ProblemSolverPopulationGenerator(TGaProblem baseProblem, IProblemSolver<TBase, TGaProblem> problemSolver)
+	{
+		BaseProblem = baseProblem;
+		ProblemSolver = problemSolver;
+	}
+
+	public IEnumerable<Genotype> Generate(int count)
+	{
+		for (var i = 0; i < count; i++)
+		{
+			yield return BaseProblem.Coder.Encode(ProblemSolver.FindSolution(BaseProblem));
+		}
+	}
+}
