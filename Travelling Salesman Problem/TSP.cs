f3de9be4624e955165519858be9a51d00e39bfd7
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-06 16:59:23 +0300

    [TSP/feat] Формализовал ЗК в контексте ЭГА

---
+++ Travelling Salesman Problem/TSP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using EA;
+using EA.BaseProblem;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem;
+
+/// <summary>
+/// Представляет собой задачу коммивояжёра. Реализует
+/// интерфейс <c>IGAProblem</c>, поэтому может быть
+/// использован как базовая задача ЭГА.
+/// </summary>
+public sealed class Tsp : IGaProblem<List<int>>
+{
+	public DistanceMatrix Distances { get; }
+	public ICoder<List<int>, Genotype> Coder { get; }
+	public ICriterion<List<int>> Criterion { get; }
+
+	public Tsp(DistanceMatrix distances)
+	{
+		Distances = distances;
+		Coder = new BasicRouteCoder();
+		Criterion = new TspCriterion(distances);
+	}
+}
