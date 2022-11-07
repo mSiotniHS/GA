5ca8abbbe917ca09d4c03bf64dc1524865508b8c
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-07 20:14:04 +0300

    [TSP/feat] Реализовал инверсию (мутация)

---
+++ Travelling Salesman Problem/Mutations/Inversion.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem.Mutations;
+
+public sealed class Inversion : IMutation
+{
+	public Genotype Perform(Genotype genotype)
+	{
+		// [startIdx, endIdx)
+		var startIdx = Randomness.GetInt(genotype.Length);
+		var endIdx = Randomness.GetInt(startIdx + 1, genotype.Length + 1);
+
+		var newGenotype = new Genotype(genotype);
+
+		for (var i = startIdx; i < startIdx + (endIdx - startIdx) / 2; i++)
+		{
+			(newGenotype[i], newGenotype[endIdx - i]) = (newGenotype[endIdx - i], newGenotype[i]);
+		}
+
+		return newGenotype;
+	}
+
+	public bool GuaranteesValidGenotype => true;
+}
