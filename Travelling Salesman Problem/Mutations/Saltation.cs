cba5b542eafa205234bd6a6fee33eedf8ad72bbf
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-07 20:13:41 +0300

    [TSP/feat] Реализовал сальтацию (мутация)

---
+++ Travelling Salesman Problem/Mutations/Saltation.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using EA.Core;
+
+namespace Travelling_Salesman_Problem.Mutations;
+
+public sealed class Saltation : IMutation
+{
+	public Genotype Perform(Genotype genotype)
+	{
+		var startIdx = Randomness.GetInt(genotype.Length - 1);
+		var endIdx = Randomness.GetInt(startIdx + 1, genotype.Length);
+
+		var newGenotype = new Genotype(genotype);
+		(newGenotype[startIdx], newGenotype[endIdx]) = (newGenotype[endIdx], newGenotype[startIdx]);
+
+		return newGenotype;
+	}
+
+	public bool GuaranteesValidGenotype => true;
+}
