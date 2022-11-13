0e579246c0af791c2af8e03cb6e88f18974b6b79
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-13 15:15:35 +0300

    [TSP/clean] Убрал вывод в консоль в CxCrossover.cs

--- Travelling Salesman Problem/Crossovers/CxCrossover.cs
+++ Travelling Salesman Problem/Crossovers/CxCrossover.cs
@@ -14,7 +14,6 @@ public sealed class CxCrossover : ICrossover
 
 		// TODO если можно подобные родителям, то убрать -2; в цикле начинать с 0
 		var totalChildAmount = Convert.ToInt32(Math.Pow(2, cycleCount)) - 2;
-		Console.WriteLine(totalChildAmount);
 
 		var children = new List<Genotype>(totalChildAmount);
 
