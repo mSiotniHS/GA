5d39559f70350035250729201806ba8336b5ffcb
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-19 15:22:45 +0300

    [TSP/feat] Добавил логи для Inversion.cs

--- Travelling Salesman Problem/Mutations/Inversion.cs
+++ Travelling Salesman Problem/Mutations/Inversion.cs
@@ -7,9 +7,18 @@ public sealed class Inversion : IMutation
 {
 	public Genotype Perform(Genotype genotype)
 	{
+#if DEBUG
+		Logger.Begin(nameof(Inversion), nameof(Perform));
+		Logger.Log($"Исходный генотип: {genotype}");
+#endif
+
 		// [startIdx, endIdx)
 		var startIdx = Randomness.GetInt(genotype.Length - 1); // -1, чтобы хотя бы 2 гена было
 		var endIdx = Randomness.GetInt(startIdx + 1, genotype.Length + 1);
+
+#if DEBUG
+		Logger.Log($"[startIdx, endIdx) = [{startIdx}, {endIdx})");
+#endif
 
 		var newGenotype = new Genotype(genotype);
 
@@ -21,6 +30,11 @@ public sealed class Inversion : IMutation
 			var right = endIdx - i - 1;
 			(newGenotype[left], newGenotype[right]) = (newGenotype[right], newGenotype[left]);
 		}
+
+#if DEBUG
+		Logger.Log($"Мутированный генотип: {newGenotype}");
+		Logger.End();
+#endif
 
 		return newGenotype;
 	}
