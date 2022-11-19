b7f997f94b992b3c88b24e01c8cb10889b91113c
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-19 10:18:17 +0300

    [GA/feat] Написал RandomValidPopulationGenerator.cs, генерирующий популяцию рандомно

---
+++ EA/CommonModules/RandomValidPopulationGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EA.BaseProblem;
+using EA.Core;
+
+namespace EA.CommonModules;
+
+public sealed class RandomValidPopulationGenerator<TBase, TGaProblem> : IPopulationGenerator
+	where TGaProblem : IGaProblem<TBase>, IRandomSolutionGenerator<TBase>
+{
+	private readonly TGaProblem _baseProblem;
+
+	public RandomValidPopulationGenerator(TGaProblem baseProblem)
+	{
+		_baseProblem = baseProblem;
+	}
+
+	public IEnumerable<Genotype> Generate(int count)
+	{
+		for (var i = 0; i < count; i++)
+		{
+			yield return _baseProblem.Coder.Encode(_baseProblem.PickRandom());
+		}
+	}
+}
