650ccf0935fbce2152603b4937af78dbf108c55f
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-20 12:56:09 +0300

    [GA/feat] Определил ICopyStrategy.cs, задающий интерфейс для копирования в селекции

---
+++ EA/ICopyStrategy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using EA.Core;
+
+namespace EA;
+
+public interface ICopyStrategy
+{
+	public IEnumerable<Genotype> Copy(IList<Genotype> fund, IList<double> weights, uint count);
+}
