12f8f9bb55242ffba3a8e3b1423a44aa09e77a01
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-20 15:17:07 +0300

    [GA/feat] Теперь из Services.FindBest можно узнать приспособленность лучшей особи

--- EA/Helpers/Services.cs
+++ EA/Helpers/Services.cs
@@ -6,16 +6,27 @@ namespace EA.Helpers;
 
 public static class Services
 {
-	public static Genotype FindBest(IEnumerable<Genotype> group, Func<Genotype, int> fitness)
+	public static Genotype FindBest(IEnumerable<Genotype> group, Func<Genotype, int> fitnessFunction)
+	{
+		return FindBest(group, fitnessFunction, out _);
+	}
+
+	public static Genotype FindBest(IEnumerable<Genotype> group, Func<Genotype, int> fitnessFunction, out int fitness)
 	{
 		Genotype? best = null;
+		var bestFitness = int.MaxValue;
+
 		foreach (var genotype in group)
 		{
-			if (best is null || fitness(genotype) < fitness(best))
+			var currentFitness = fitnessFunction(genotype);
+			if (best is null || currentFitness < bestFitness)
 			{
 				best = genotype;
+				bestFitness = currentFitness;
 			}
 		}
+
+		fitness = bestFitness;
 
 		return best ?? throw new Exception($"[{nameof(Services)}/{nameof(FindBest)}] Не удалось найти лучшего");
 	}
