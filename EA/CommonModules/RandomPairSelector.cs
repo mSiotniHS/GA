266ce1db5550923df6b7bac72b5956a3ed8177ef
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-20 12:57:43 +0300

    [GA/feat] Добавил логи для RandomPairSelector.cs

--- EA/CommonModules/RandomPairSelector.cs
+++ EA/CommonModules/RandomPairSelector.cs
@@ -9,8 +9,16 @@ public sealed class RandomPairSelector : IPairSelector
 {
 	public IEnumerable<(Genotype, Genotype)> Select(IEnumerable<Genotype> population)
 	{
+#if DEBUG
+		Logger.Begin(nameof(RandomPairSelector), nameof(Select));
+#endif
+
 		var populationList = population.ToList();
 		var pairs = new List<(Genotype, Genotype)>();
+
+#if DEBUG
+		Logger.Log($"Получили популяцию:\n{string.Join('\n', populationList)}");
+#endif
 
 		while (populationList.Count != 0)
 		{
@@ -22,8 +30,16 @@ public sealed class RandomPairSelector : IPairSelector
 			var second = populationList[secondIdx];
 			populationList.RemoveAt(secondIdx);
 
+#if DEBUG
+			Logger.Log($"Сформировали пару: {first} и {second}");
+#endif
+
 			pairs.Add((first, second));
 		}
+
+#if DEBUG
+		Logger.End();
+#endif
 
 		return pairs;
 	}
