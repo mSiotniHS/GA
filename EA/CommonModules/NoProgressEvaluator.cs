fe36e0d94ab2f9bf54cb77f783bd7ab2a60c4d88
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-21 08:09:10 +0300

    [GA/feat] Реализовал условие останова, когда приспособленность не улучшается

---
+++ EA/CommonModules/NoProgressEvaluator.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using EA.Helpers;
+
+namespace EA.CommonModules;
+
+/// <summary>
+/// Завершает работу ЭГА, если приспособленность лучших особей
+/// "n" поколений не улучшается
+/// </summary>
+/// <typeparam name="TBaseType"></typeparam>
+public sealed class NoProgressEvaluator<TBaseType> : IEvaluationStrategy<TBaseType>
+{
+	private readonly uint _maxNoProgressCount;
+	private uint _noProgressCount;
+	private int _lastBestFitness;
+
+	public NoProgressEvaluator(uint maxNoProgressCount)
+	{
+		_maxNoProgressCount = maxNoProgressCount;
+		_noProgressCount = 0;
+		_lastBestFitness = int.MaxValue;
+	}
+
+	public bool ShouldWork(GaManager<TBaseType> state)
+	{
+		if (state.Statistics.Trace.Count == 0) return true;
+
+		if (state.Statistics.Trace.Count == 1)
+		{
+			Services.FindBest(state.Statistics.Trace[0], state.Phenotype, out _lastBestFitness);
+			return true;
+		}
+
+		Services.FindBest(state.Statistics.Trace.Last(), state.Phenotype, out var bestFitness);
+
+		if (bestFitness < _lastBestFitness)
+		{
+			_lastBestFitness = bestFitness;
+			_noProgressCount = 0;
+			return true;
+		}
+
+		_noProgressCount++;
+		return _noProgressCount < _maxNoProgressCount;
+	}
+
+	public void Reset()
+	{
+		_lastBestFitness = int.MaxValue;
+		_noProgressCount = 0;
+	}
+}
