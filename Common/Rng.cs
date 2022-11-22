d5449b33440a98ec58d56f8caf5e58af2e819174
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-22 23:19:50 +0300

    [Common/feat] Определил класс Rng.cs, реализующий интерфейс IRng

---
+++ Common/Rng.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common;
+
+public sealed class Rng : IRng
+{
+	private static readonly Random Random = new();
+
+	public int GetInt() => Random.Next();
+	public int GetInt(int max) => Random.Next(max);
+	public int GetInt(int min, int max) => Random.Next(min, max);
+	public double GetDouble() => Random.NextDouble();
+}
