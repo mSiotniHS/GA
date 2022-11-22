ce36dc7afbb9d405c31c744461444ce8cdd44b07
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-22 23:19:15 +0300

    [Common/feat] Определил IRng.cs, определяющий интерфейс RNG

---
+++ Common/IRng.cs
@@ -0,0 +1,18 @@
+﻿namespace Common;
+
+public interface IRng
+{
+	public int GetInt();
+
+	/// <param name="max">Не включительно</param>
+	/// <returns>int в [0, max)</returns>
+	public int GetInt(int max);
+
+	/// <param name="min">Включительно</param>
+	/// <param name="max">Не включительно</param>
+	/// <returns>int в [min, max)</returns>
+	public int GetInt(int min, int max);
+
+	/// <returns>double в [0, 1)</returns>
+	public double GetDouble();
+}
