8461f37e7ba46553586f368278d49291620d04ec
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-27 13:36:35 +0300

    [TSP.Tests/feat] Поправил тест для Inversion.cs

--- TSP.Tests/InversionTests.cs
+++ TSP.Tests/InversionTests.cs
@@ -1,3 +1,4 @@
+using Common;
 using EA.Core;
 using Travelling_Salesman_Problem.Mutations;
 
@@ -5,29 +6,42 @@ namespace TSP.Tests;
 
 public sealed class InversionTests
 {
-	private readonly Inversion _inversion;
-
-	public InversionTests(Inversion inversion)
-	{
-		_inversion = inversion;
-	}
-
 	[Theory]
-	[MemberData(nameof(PpsbcTestData))]
-	public void PurePerformShouldBehaveCorrectly(Genotype genotype, int start, int end, Genotype expected)
+	[MemberData(nameof(InversionShouldWorkTestData))]
+	public void InversionShouldWork(int[] range, int?[] rawGenotype, int?[] rawExpected)
 	{
-		// var mutant = _inversion.PurePerform(genotype, start, end);
+		var rng = new PseudoRng(range, Array.Empty<double>());
+		var inversion = new Inversion(rng);
 
-		// Assert.Equal(expected, mutant);
+		var genotype = new Genotype(rawGenotype);
+		var expected = new Genotype(rawExpected);
+
+		var actual = inversion.Perform(genotype);
+
+		Assert.Equal(expected.ToFilledArray(), actual.ToFilledArray());
 	}
 
-	private static IEnumerable<object[]> PpsbcTestData()
+	public static IEnumerable<object[]> InversionShouldWorkTestData()
 	{
 		yield return new object[]
 		{
-			new Genotype(new int?[] {1, 4, 3, 2, 5}),
-			1, 4,
-			new Genotype(new int?[] {1, 2, 3, 4, 5})
+			new[] { 2, 5 },
+			new int?[] {1, 2, 3, 4, 5, 6, 7},
+			new int?[] {1, 2, 5, 4, 3, 6, 7}
+		};
+
+		yield return new object[]
+		{
+			new[] { 1, 3 },
+			new int?[] {1, 2, 3, 4, 5, 6},
+			new int?[] {1, 3, 2, 4, 5, 6}
+		};
+
+		yield return new object[]
+		{
+			new[] { 2, 6 },
+			new int?[] {1, 2, 3, 4, 5, 6, 7},
+			new int?[] {1, 2, 6, 5, 4, 3, 7}
 		};
 	}
 }
