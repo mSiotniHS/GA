05956f45be8a186f3dd4f5bff80093e5198a515b
Artyom Abramyan <artemabramyan7@icloud.com>
2022-12-05 00:18:58 +0300

    [GA/log] Добавил логи для OutbreedingPairSelector.cs

--- EA/CommonModules/OutbreedingPairSelector.cs
+++ EA/CommonModules/OutbreedingPairSelector.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Common;
 using EA.Core;
 
 namespace EA.CommonModules;
@@ -15,7 +16,11 @@ public sealed class OutbreedingPairSelector : IPairSelector
 
 	public IEnumerable<(Genotype, Genotype)> Select(IEnumerable<Genotype> population)
 	{
+		Logger.Begin(nameof(OutbreedingPairSelector), nameof(Select));
+
 		var populationList = population.ToList();
+		Logger.Log("Получили популяцию:\n" + string.Join('\n', populationList));
+
 		var pairs = new List<(Genotype, Genotype)>();
 
 		while (populationList.Count != 0)
@@ -23,30 +28,46 @@ public sealed class OutbreedingPairSelector : IPairSelector
 			const int firstIdx = 0;
 			var firstParent = populationList[firstIdx];
 
+			Logger.Log($"Ищем пару для {firstParent} среди популяции:\n" + string.Join('\n', populationList));
+
 			var furthestIdx = -1;
 			var furthestDistance = -1;
 
 			for (var i = firstIdx + 1; i < populationList.Count; i++)
 			{
+				Logger.Log($"Наибольшее расстояние: {furthestDistance}");
+
 				var candidate = populationList[i];
+				Logger.Log($"Кандидат: {candidate}");
+
 				var candidateDistance = firstParent.DistanceTo(candidate);
+				Logger.Log($"Расстояние до кандидата: {candidateDistance}");
 
 				if (candidateDistance > furthestDistance)
 				{
+					Logger.Log("Расстояние до кандидата больше известного, сохраняем");
 					furthestIdx = i;
 					furthestDistance = candidateDistance;
 
 					if (furthestDistance >= _minDifference)
 					{
+						Logger.Log("Расстояние до кандитата больше, чем минимальное. Выходим из цикла");
 						break;
 					}
 				}
 			}
 
 			pairs.Add((firstParent, populationList[furthestIdx]));
+
+			Logger.Log($"Добавили пару: {firstParent} и {populationList[furthestIdx]}");
+			Logger.Log($"Расстояние: {furthestDistance}");
+
 			populationList.RemoveAt(firstIdx);
 			populationList.RemoveAt(furthestIdx);
 		}
+
+		Logger.Log("Получили пары:\n" + string.Join('\n', pairs));
+		Logger.End();
 
 		return pairs;
 	}
