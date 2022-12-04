6eeecb242fc79877001cb176e84066b05a4ddfd6
Artyom Abramyan <artemabramyan7@icloud.com>
2022-12-05 00:19:18 +0300

    [GA/refac] Обновил логи для WithoutReturnCopy.cs

--- EA/CommonModules/WithoutReturnCopy.cs
+++ EA/CommonModules/WithoutReturnCopy.cs
@@ -16,10 +16,8 @@ public sealed class WithoutReturnCopy : ICopyStrategy
 
 	public IEnumerable<Genotype> Copy(IList<Genotype> fund, IList<double> weights, uint count)
 	{
-#if DEBUG
 		Logger.Begin(nameof(WithoutReturnCopy), nameof(Copy));
 		Logger.Log($"Особи и веса:\n{string.Join('\n', fund.Zip(weights, (w, f) => $"*) {w} - {f}"))}");
-#endif
 
 		var copiedFund = fund.Select(x => new Genotype(x)).ToList();
 		var copiedWeights = weights.ToList();
@@ -27,16 +25,12 @@ public sealed class WithoutReturnCopy : ICopyStrategy
 		for (var i = 0; i < count; i++)
 		{
 			yield return Roulette.Spin(_rng, copiedFund, copiedWeights, out var idx);
-
-#if DEBUG
 			Logger.Log($"Выбрали {copiedFund[idx]}");
-#endif
 
 			copiedFund.RemoveAt(idx);
 			copiedWeights.RemoveAt(idx);
 		}
-#if DEBUG
+
 		Logger.End();
-#endif
 	}
 }
